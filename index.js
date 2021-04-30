e69e82d9b121c7ce3cacabee61b049510f309335
Drew <59685900+Drewery-ctrl@users.noreply.github.com>
2021-04-30 15:15:16 -0500

    Setup app to  running state

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Start your es6 scripts here
+import express from "express";
+const app = express();
+
+let PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Up and running with graphql course");
+});
+
+app.listen(PORT, () => console.log(`App running in http://localhost:${PORT}`));
